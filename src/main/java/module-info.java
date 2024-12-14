5c787080aa266844686b11fdf7e57910a0030ee3
Laura Celeste Berrio <laura.berrio@correounivalle.edu.co>
2024-12-14 10:10:31 -0500

    Fix current player index for turns

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.example.cincuentazo {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.junit.jupiter.api;
 
     opens com.example.cincuentazo to javafx.fxml;
     opens com.example.cincuentazo.controllers to javafx.fxml;
